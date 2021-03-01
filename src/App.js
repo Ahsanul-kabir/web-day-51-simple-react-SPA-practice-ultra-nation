f52549bd9cf534a33a123954d1ffe829cc14f4e6
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-03-01 10:59:11 +0600

    2. display loaded data in a simple way added

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ function App() {
   useEffect(()=>{
     fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
-    .then(data=>setCountries(data))
+    .then(data=>{
+      setCountries(data);
+      console.log(data);
+      const names =  data.map(country => country.name)
+      console.log(names);})
   },[])
   return (
     <div >
       <h1>Country Loaded: {countries.length}</h1>
+      <ul>
+        {
+          countries.map(country => <li>{country.name}</li>)
+        }
+      </ul>
     </div>
   );
 }
