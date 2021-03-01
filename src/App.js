7dd11fc9478e3423db09785eaf753cf5c757e8fc
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-03-01 10:22:22 +0600

    1. Ultra Nation load countries data using useEffect added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries,setCountries] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data=>setCountries(data))
+  },[])
   return (
     <div >
-
+      <h1>Country Loaded: {countries.length}</h1>
     </div>
   );
 }
