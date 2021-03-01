7f4b29d505e58d4d9391fa0b0a09ee652b16c9a5
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-03-01 11:40:37 +0600

    3. Create child component and pass data addd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Country from './components/Country.js/Country';
 
 function App() {
-  const [countries,setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => res.json())
-    .then(data=>{
-      setCountries(data);
-      console.log(data);
-      const names =  data.map(country => country.name)
-      console.log(names);})
-  },[])
+      .then(res => res.json())
+      .then(data => {
+        setCountries(data);
+        console.log(data);
+        const names = data.map(country => country.name)
+        console.log(names);
+      })
+  }, [])
   return (
     <div >
       <h1>Country Loaded: {countries.length}</h1>
       <ul>
         {
-          countries.map(country => <li>{country.name}</li>)
+          countries.map(country => <Country name={country.name}></Country>)
         }
       </ul>
     </div>
